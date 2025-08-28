20793e416665d604ed146d9440355f5516473820
Coolest-Tomato <zash8158@colorado.edu>
2025-08-28 10:41:36 -0600

    workshop2

--- Workshop2_copy_20250828101705/Workshop2_copy_20250828101705.ino
+++ Workshop2_copy_20250828101705/Workshop2_copy_20250828101705.ino
@@ -1,7 +1,8 @@
 #include <WiFi.h>
-#include "DHT.h"
+// #include "DHT.h"
 #include "ThingSpeak.h"
-#include <Adafruit_BME280.h>
+#include <SPI.h>
+#include <Adafruit_BME680.h>
 #include <Adafruit_Sensor.h>
 
 // Setting WiFi 
@@ -9,19 +10,42 @@ const char* ssid = "CapstoneWifi2";
 const char* password = "RuleNumber9";
 
 // Setting ThingSpeak
-unsigned long myChannelNumber = YOUR_CHANNEL_NUMBER;
-const char * myWriteAPIKey = "YOUR_WRITE_API_KEY";
+unsigned long myChannelNumber = 3051754;
+const char * myWriteAPIKey = "5B7F31T5R7TPH4MC";
 
 // Setting DHT Sensor
-#define DHTPIN 4        // 센서를 연결한 ESP32 핀 
-#define DHTTYPE DHT22   // DHT11 사용 시 DHT11로 변경
-DHT dht(DHTPIN, DHTTYPE);
+// #define DHTPIN 4        // ESP32 pins connected to sensor 
+// #define DHTTYPE DHT22   // switch to DHT11 when using DHT
+// DHT dht(DHTPIN, DHTTYPE);
+
+// Sensors 
+
 
 WiFiClient client;
 
+// Create a sensor object
+Adafruit_BME680 bme; // I2C
+//Adafruit_BME680 bme(BME_CS); // hardware SPI
+//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+
 void setup() {
   Serial.begin(115200);
-  dht.begin();
+  //dht.begin();
+
+  while (!Serial);
+  Serial.println(F("BME680 async test"));
+
+  if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -37,16 +61,64 @@ void setup() {
 }
 
 void loop() {
-  // read data from sensor
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(true); // true → (°F), false → (°C)
+  // // read data from sensor
+  // float h = dht.readHumidity();
+  // float t = dht.readTemperature(true); // true → (°F), false → (°C)
 
-  // check sensor error
-  if (isnan(h) || isnan(t)) {
-    Serial.println("DHT fail to read sensor");
-    delay(2000);
+  // // check sensor error
+  // if (isnan(h) || isnan(t)) {
+  //   Serial.println("DHT fail to read sensor");
+  //   delay(2000);
+  //   return;
+  // }
+
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
     return;
   }
+  Serial.print(F("Reading started at "));
+  Serial.print(millis());
+  Serial.print(F(" and will finish at "));
+  Serial.println(endTime);
+
+  Serial.println(F("You can do other work during BME680 measurement."));
+  delay(50); // This represents parallel work.
+  // There's no need to delay() until millis() >= endTime: bme.endReading()
+  // takes care of that. It's okay for parallel work to take longer than
+  // BME680's measurement time.
+
+  // Obtain measurement results from BME680. Note that this operation isn't
+  // instantaneous even if milli() >= endTime due to I2C/SPI latency.
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
+  Serial.print(F("Reading completed at "));
+  Serial.println(millis());
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(F(" hPa"));
+
+  Serial.print(F("Humidity = "));
+  Serial.print(bme.humidity);
+  Serial.println(F(" %"));
+
+  Serial.print(F("Gas = "));
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(F(" KOhms"));
+
+  Serial.print(F("Approx. Altitude = "));
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(F(" m"));
+
+  Serial.println();
+  delay(2000);
 
   Serial.print("Temperature (°F): ");
   Serial.println(t);
@@ -54,8 +126,8 @@ void loop() {
   Serial.println(h);
 
   // Upload to ThingSpeak (Using Field1, Field2 of Channel)
-  ThingSpeak.setField(1, t);
-  ThingSpeak.setField(2, h);
+  ThingSpeak.setField(1, bme.temperature);
+  ThingSpeak.setField(2, bme.humidity);
 
   int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
   if (x == 200) {
@@ -64,5 +136,5 @@ void loop() {
     Serial.println("Failure Upload. HTTP Error code: " + String(x));
   }
 
-  delay(600000);
+  delay(600000); 
 }
